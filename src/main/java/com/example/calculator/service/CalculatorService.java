a5ffc37850341da9592f02721264c39f7850bbc2
LexImmel <juffin91@gmail.com>
2024-09-16 03:01:40 +0500

    initial

--- src/main/java/com/example/calculator/service/CalculatorService.java
+++ src/main/java/com/example/calculator/service/CalculatorService.java
@@ -7,12 +7,15 @@ public class CalculatorService {
     public Integer plus(Integer a, Integer b) {
         return a + b;
     }
+
     public Integer minus(Integer a, Integer b) {
         return a - b;
     }
+
     public Integer multiply(Integer a, Integer b) {
         return a * b;
     }
+
     public double divide(Integer a, Integer b) {
         return (double) a / b;
     }
